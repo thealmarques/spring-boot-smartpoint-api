785ec13814769e2aee14508c16befc998d114565
Andre Marques <utilizador@MBP-de-Andre.Home>
2020-06-03 16:43:48 +0100

    Added spring boot initial repo

---
+++ src/main/java/com/course/pontointeligente/PontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.course.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/pontointeligente/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.pontointeligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
