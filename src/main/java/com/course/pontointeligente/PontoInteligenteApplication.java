1be69021160ea60dcb97f34f140c862a5584f16e
Andre Marques <utilizador@MBP-de-Andre.Home>
2020-06-18 15:42:01 +0100

    Added ehcache

--- src/main/java/com/course/pontointeligente/PontoInteligenteApplication.java
+++ src/main/java/com/course/pontointeligente/PontoInteligenteApplication.java
@@ -3,8 +3,10 @@ package com.course.pontointeligente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
